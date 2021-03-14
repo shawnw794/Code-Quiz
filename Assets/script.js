ab948b8a7294610c18f4cb77067d09c950bc8c15
Shawn Williams <shawnw794@gmail.com>
2021-03-13 16:06:52 -0800

    created questions for the quiz.

---
+++ Assets/script.js
@@ -0,0 +1,41 @@
+var questions = [{
+    "question": "Who are the principle father-son duo for this movie?",
+    "answers": ["Pete and P.J.", "Goofy and Max", "George and Astro", "Barney and Bam-Bam"],
+    "correctA": 1
+}, {
+    "question": "Who is Max's high school crush?",
+    "answers": ["Minnie", "Juliette", "Roxanne", "Marge"],
+    "correctA": 2
+}, {
+    "question": "Where does Goofy decides to take Max for a family trip?",
+    "answers": ["Las Vegas, Nevada", "Lake Destiny, Idaho", "Los Angeles, California", "Dallas, Texas"],
+    "correctA": 1
+}, {
+    "question": "Goofy's family trip was inspired for his fondess for ______.",
+    "answers": ["Camping", "Hiking", "Bird Watching", "Fishing"],
+    "correctA": 3
+}, {
+    "question": "What was the name of the soup that Goofy and Max while traveling",
+    "answers": ["Hi, Dad Soup", "Chicken Soup", "Vegetable Soup", "Goof Troop Soup"],
+    "correctA": 0
+}, {
+    "question": "What entertainment did Goofy take Max to that reminded him of a young Max?",
+    "answers": ["Wrestlemania", "Monster Truck Rally", "Nascar Talladega", "Powerline Concert"],
+    "correctA": 1
+}, {
+    "question": "Who is the name of the artist(s) that Max imitates in the beginning of the movie?",
+    "answers": ["Powerline", "California Raisins", "Brass Knuckleheads", "The Rattling Rocks"],
+    "correctA": 0
+}, {
+    "question": "What was the name of the fishing technique that Goofy taught max?",
+    "answers": ["The Zipline", "Fly Fisherman", "The Perfect Cast", "Saffron & Paprika"],
+    "correctA": 2
+}, {
+    "question": "What happens when Goofy puts Max in charge telling directions for the road trip?",
+    "answers": ["Max guided them safely to Lake Detiny.", "Max loses the map during the trip.", "Max was never put in charge of the map.", "Max changed the directions to get to a concert."],
+    "correctA": 3
+}, {
+    "question": "What song did both Goofy and Max dance to on stage with Powerline?",
+    "answers": ["Stand Out", "Eye to Eye", "Cosmos Rocker", "Je T'Aime"],
+    "correctA": 1
+}]
